8b441bea01f79e4fbc977f78cb8437d41fe3d016
Guksu <yung3152@daum.net>
2021-11-23 13:04:10 +0900

    react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
-import logo from "./logo.svg";
-
-const animation = keyframes`
-  from {
-    transform:rotate(0deg);
-  }
-  to{
-    transform:rotate(360deg);
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${animation} 1s liner;
-`;
+import ToDoList from "./TodoList";
 
 function App() {
-  return <Box />;
+  return (
+    <>
+      <ToDoList />
+    </>
+  );
 }
-
 export default App;
