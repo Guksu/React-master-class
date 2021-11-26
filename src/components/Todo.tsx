6159734db19da7d9616efcdb8f3d386e57d7fcce
Guksu <yung3152@daum.net>
2021-11-26 18:11:07 +0900

    recoil연습

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,42 @@
-import { IToDo } from "../atom";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atom";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  // onClick이 arg가 필요한 경우엔 아래와 같이 함수를 다시 선언해야한다.
+  // onClick={() => onClick("DOING")}
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldTodos) => {
+      const targetIndex = oldTodos.findIndex((todo) => (todo.id = id));
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldTodos.slice(0, targetIndex),
+        newToDo,
+        ...oldTodos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
