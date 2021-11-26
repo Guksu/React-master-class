816fc1e31f783ace3e3750141068c874254e6ea3
Guksu <yung3152@daum.net>
2021-11-27 01:17:59 +0900

    recoil-TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,36 +1,34 @@
-import { useRecoilValue } from "recoil";
-import { toDoSelector } from "../atom";
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { Categories, categoryState, toDoSelector } from "../atom";
 import CreateToDo from "./CreateTodoList";
 import ToDo from "./Todo";
 
 function ToDoList() {
-  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    // setCategory는 categories 타입을 받아야 하지만
+    // ts의 option의 value는 string타입이기 때문에
+    // any 타입으로 변환해준다.
+    setCategory(event.currentTarget.value as any);
+  };
+
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <form>
+        <select value={category} onInput={onInput}>
+          <option value={Categories.TO_DO}>To Do</option>
+          <option value={Categories.DOING}>Doing</option>
+          <option value={Categories.DONE}>Done</option>
+        </select>
+      </form>
       <CreateToDo />
-      <h2>To Do</h2>
-      <ul>
-        {toDo.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Doing</h2>
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Done</h2>
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
+      {toDos?.map((todo) => (
+        <ToDo key={todo.id} {...todo} />
+      ))}
     </div>
   );
 }
