dd2bb5563df291917980def40b18a4b26c5a3887
Guksu <yung3152@daum.net>
2021-12-10 17:06:46 +0900

    DragDrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import ToDoList from "./components/TodoList";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function App() {
+  const onDragEnd = () => {};
   return (
     <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div>
+          {/* droppable의 children은 함수형식이어야 한다 */}
+          <Droppable droppableId="1">
+            {(provide) => (
+              <ul ref={provide.innerRef} {...provide.droppableProps}>
+                <Draggable draggableId="first" index={0}>
+                  {(provide) => (
+                    <li ref={provide.innerRef} {...provide.draggableProps}>
+                      <span {...provide.dragHandleProps}>☝</span>
+                      one
+                    </li>
+                  )}
+                </Draggable>
+                <Draggable draggableId="second" index={1}>
+                  {(provide) => (
+                    <li ref={provide.innerRef} {...provide.draggableProps}>
+                      <span {...provide.dragHandleProps}>☝</span>
+                      two
+                    </li>
+                  )}
+                </Draggable>
+              </ul>
+            )}
+          </Droppable>
+        </div>
+      </DragDropContext>
       <ToDoList />
     </>
   );
